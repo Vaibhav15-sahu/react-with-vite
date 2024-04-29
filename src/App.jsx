79c736b6e6949e1f0a3c203805dbc5a10f117ff7
Vaibahv Sahu <vaibhavsahu0115@gmail.com>
2024-04-29 18:58:23 +0530

    Dynamic Counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import './App.css'
 
 function App() {
   
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [steps, setSteps] = useState(1);
 
   const Add = () => {
-    setCount(count + 1)
+    setCount(count + steps)
   }
 
   const Delete = () => {
-    setCount(count - 1)
+    setCount(count - steps)
+  }
+
+  const update = (e) => {
+    setSteps(parseInt(e.target.value));
   }
 
   return (
     <>
       <h1>Counter : {count} </h1>
+      <br />
+      <h3>Number of Steps : </h3>
+      <input onChange={update} ></input>
       <br />
       <button onClick={Add}>Add </button> {" "} 
       <button onClick={Delete}>Delete</button>
